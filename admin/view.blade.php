596a6f1799f7e1cc66ea21bb89e2ebd72db6b308
purple <hello@prpl.wtf>
2023-08-03 21:25:24 +0200

    Change icon

--- admin/view.blade.php
+++ admin/view.blade.php
@@ -4,7 +4,7 @@
     <!-- Overview -->
     <div class="box">
       <div class="box-header with-border">
-        <h3 class="box-title"><i class='bx bxs-dashboard' style='margin-right:5px;'></i>Overview</h3>
+        <h3 class="box-title"><i class='bx bx-command' style='margin-right:5px;'></i>Overview</h3>
       </div>
       <div class="box-body">
         <p>You are currently running version <code>^#version#^</code>.</p>
