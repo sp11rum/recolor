dbce3a0e7a546fd583e1ac033873a69210a6424c
Jack G <email@sp11rum.com>
2023-08-03 20:18:57 +0100

    Update view.blade.php

--- admin/view.blade.php
+++ admin/view.blade.php
@@ -7,6 +7,6 @@
         <h3 class="box-title"><i class='bx bxs-dashboard' style='margin-right:5px;'></i>Overview</h3>
       </div>
       <div class="box-body">
-        <p>Thank you for purchasing Recolor. You are currently running version <code>^#version#^</code>.</p>
+        <p>You are currently running version <code>^#version#^</code>.</p>
       </div>
     </div>
